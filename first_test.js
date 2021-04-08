c9b8573af183e5879bfa059080916a32635a1f91
Milad <milad.ranjbar14@gmail.com>
2021-04-08 13:58:29 +0430

    new js file

---
+++ first_test.js
@@ -0,0 +1,10 @@
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const Http = new XMLHttpRequest();
+
+const url  = 'https://jsonplaceholder.typicode.com/posts';
+Http.open("GET",url);
+Http.send();
+Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+}
